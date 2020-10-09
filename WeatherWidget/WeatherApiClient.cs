45aa71406128957eb2a9445583787dec19663269
ai.shlauzer <ai.shlauzer@tensor.ru>
2020-10-10 00:05:03 +0700

    заменил HTTP-клиент на RestSharp

--- WeatherWidget/WeatherApiClient.cs
+++ WeatherWidget/WeatherApiClient.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Collections.Generic;
+
+using RestSharp;
 
 namespace WeatherWidget
 {
@@ -11,29 +10,19 @@ namespace WeatherWidget
         const string URL = "https://api.openweathermap.org/data/2.5/forecast";
         const string PARAMS = "?q={0}&mode=xml&appid={1}";
 
-        // создание HTTP-клиента
-        HttpClient client = new HttpClient();
         // создание XML-парсера
         XmlParser xmlParser = new XmlParser();
 
-        public WeatherApiClient()
-        {
-            // установка базового адреса интернет-ресурса
-            client.BaseAddress = new Uri(URL);
-            // установка заголовков запроса
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-        }
+        // REST-клиент
+        readonly RestClient client = new RestClient(URL) { Timeout = -1 };
 
         public List<WeatherData> LoadWeatherForecast(string city = "Novosibirsk")
         {
-            List<WeatherData> weatherForecast = null;
             string requestUri = string.Format(PARAMS, city, API_KEY);
-            HttpResponseMessage response = client.GetAsync(requestUri).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                string responseString = response.Content.ReadAsStringAsync().Result;
-                weatherForecast = xmlParser.ParseWeatherForecast(responseString);
-            }
+            var request = new RestRequest(requestUri, Method.GET) { RequestFormat = DataFormat.Xml };
+            IRestResponse response = client.Execute(request);
+
+            List<WeatherData> weatherForecast = xmlParser.ParseWeatherForecast(response.Content);
             return weatherForecast;
         }
     }
